92c646cd918697a8199a0e10b3314e6bdeb2e643
Juurinen Juho T <juho.juurinen@gmail.com>
2018-01-22 23:36:24 +0200

    exercise 3.4

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ app.get("/api/persons/:id", (req, res) => {
   }
 })
 
+app.delete("/api/persons/:id", (req, res) => {
+  let id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Now listening to requests on port ${PORT}`)
