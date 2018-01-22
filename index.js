05167b604ebdf9833c46bc4e07aee1d8b2590dbf
Juurinen Juho T <juho.juurinen@gmail.com>
2018-01-23 00:02:42 +0200

    exercise 3.6

--- index.js
+++ index.js
@@ -4,39 +4,45 @@ const bodyParser = require("body-parser")
 
 let persons = [
   {
-    "id": 1,
-    "name": "Spengebeb Squrupunts",
-    "number": "+1 42 666 1337"
+    id : 1,
+    name : "Spengebeb Squrupunts",
+    number : "+1 42 666 1337"
   },
   {
-    "id": 2,
-    "name": "Chuck Norris",
-    "number": "none of your business"
+    id : 2,
+    name : "Chuck Norris",
+    number : "none of your business"
   },
   {
-    "id": 3,
-    "name": "Spengebeb Chucknorris",
-    "number": "+358 40 123 4567"
+    id : 3,
+    name : "Spengebeb Chucknorris",
+    number : "+358 40 123 4567"
   },
   {
-    "id": 4,
-    "name": "Spengebeb Nuckchorris",
-    "number": "N/A"
+    id : 4,
+    name : "Spengebeb Nuckchorris",
+    number : "N/A"
   },
   {
-    "id": 5,
-    "name": "Jackiechuck Channorris",
-    "number": "+86 139 1234 5678"
+    id : 5,
+    name : "Jackiechuck Channorris",
+    number : "+86 139 1234 5678"
   },
   {
-    "id": 6,
-    "name": "Chackie Jan",
-    "number": "+1 12 358 1321"
+    id : 6,
+    name : "Chackie Jan",
+    number : "+1 12 358 1321"
   }
 ]
 
 const generateId = () => {
-  return Math.floor(Math.random() * 1000000000)
+  let id = Math.floor(Math.random() * 1000000000)
+
+  if (persons.find(p => p.id === id)) {
+    return generateId()
+  } else {
+    return id
+  }
 }
 
 app.use(bodyParser.json())
@@ -64,11 +70,26 @@ app.get("/api/persons/:id", (req, res) => {
 })
 
 app.post("/api/persons", (req, res) => {
-  let person = req.body
-  person.id = generateId()
+  let body = req.body
+
+  if (req.body.name === undefined) {
+    return res.status(400).json( { error : "name missing" } )
+  }
+  if (req.body.number === undefined) {
+    return res.status(400).json( { error : "number missing" } )
+  }
+  if (persons.map(p => p.name.toLowerCase())
+             .find(n => n == req.body.name.toLowerCase())) {
+    return res.status(400).json( { error : "name already in use" } )
+  }
+
+  let person = {
+    id : generateId(),
+    name : req.body.name,
+    number : req.body.number
+  }
 
   persons = persons.concat(person)
-
   res.json(person)
 })
 
